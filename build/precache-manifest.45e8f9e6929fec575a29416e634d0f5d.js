7b17f89a3bc06d9a1620a2284d5f52e1a44ce711
Musadiq Peerzada <musadiq.peerzada@smartserv.io>
2022-02-13 01:38:40 +0530

    changes in package.json

---
+++ build/precache-manifest.45e8f9e6929fec575a29416e634d0f5d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b6da6242f93f3a528f83efc0516dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20800fdba89de94e4759",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "2314834967cf1108eac9",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "867950cabc0b54e56db0",
+    "url": "/static/css/main.66262938.chunk.css"
+  },
+  {
+    "revision": "fbd3e49ac55b30a04fa9",
+    "url": "/static/js/2.caf4e888.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.caf4e888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20800fdba89de94e4759",
+    "url": "/static/js/3.fafb27e7.chunk.js"
+  },
+  {
+    "revision": "2314834967cf1108eac9",
+    "url": "/static/js/4.6508a8d2.chunk.js"
+  },
+  {
+    "revision": "867950cabc0b54e56db0",
+    "url": "/static/js/main.f9ed2002.chunk.js"
+  },
+  {
+    "revision": "ca6ae2b9f4ab95b6d548",
+    "url": "/static/js/runtime-main.2ff9d66b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a41e2a04ed67b55f2be0a8a15db31341",
+    "url": "/static/media/aws-logo.a41e2a04.png"
+  },
+  {
+    "revision": "47af6e20666c3043af01b4e6dfb1990a",
+    "url": "/static/media/coep-logo.47af6e20.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "719681cc2e3b33456e2e702e9244e078",
+    "url": "/static/media/coursera-logo-new.719681cc.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5f1cef0886d49d37e2970b63e4e9c1ce",
+    "url": "/static/media/hongkonglogo.5f1cef08.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "0001126f990130dc3947ad45d33773ba",
+    "url": "/static/media/open-source-badge.0001126f.png"
+  },
+  {
+    "revision": "a02a58e934ba4eefd43679aa3aa88f6b",
+    "url": "/static/media/persistent-logo.a02a58e9.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "671b05db55c4044a168df43038177da8",
+    "url": "/static/media/smartserv-logo.671b05db.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "dd6bf929d345659531d73675855267f6",
+    "url": "/static/media/thelinuxfoundation-logo.dd6bf929.png"
+  }
+]);
